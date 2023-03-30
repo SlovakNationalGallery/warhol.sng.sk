56c305996607df84127054d1135465e4a4f323bd
Igor Rjabinin <igor.rjabinin@gmail.com>
2023-03-30 16:32:30 +0200

    init - add vue, tailwind and electron

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+  <h1 class="mb-4 text-3xl font-bold text-blue-600 underline">
+    Tailwind CSS 3 with vue js 3
+  </h1>
+  <button class="px-6 py-2 text-sm text-green-100 bg-indigo-600 rounded">Next</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>

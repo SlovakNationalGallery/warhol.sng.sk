0976fdb22aa033d98470d9b55b3d2eb8913aa651
FrantisekMichalSebestyen <sebestyen.frantisek.michal@gmail.com>
2023-03-31 15:14:30 +0200

    import p5js create p5 canvas with screen print canvas

---
+++ src/components/InteractionCanvas.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="interaction-canvas"></div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+
+/* eslint-disable */
+onMounted(() => {
+  // NOTE: Use p5 as an instance mode
+  const P5 = require("p5");
+  const script = function (p5) {
+    const P5_CANVAS = {
+      width: 800,
+      height: 800
+    }
+
+    const SCREEN_PRINT_CANVAS = {
+      width: 200,
+      height: 325,
+      posX: (P5_CANVAS.width - 200) / 2,
+      posY: (P5_CANVAS.height - 325) / 2
+    }
+
+    // NOTE: Set up is here
+    p5.setup = (_) => {
+      const canvas = p5.createCanvas(P5_CANVAS.width, P5_CANVAS.height);
+      canvas.parent("interaction-canvas");
+    };
+
+    p5.draw = (_) => {
+      p5.background(220);
+      p5.fill(255);
+      p5.rect(
+        SCREEN_PRINT_CANVAS.posX,
+        SCREEN_PRINT_CANVAS.posY,
+        SCREEN_PRINT_CANVAS.width,
+        SCREEN_PRINT_CANVAS.height
+      );
+    };
+  };
+  new P5(script);
+})
+</script>
--- src/views/CreateSoup.vue
+++ src/views/CreateSoup.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
-  <h1 class="mb-4 text-3xl font-bold text-blue-600 underline">
-    Interaction screen
-  </h1>
-  <router-link to="/" class="px-6 py-2 text-sm text-green-100 bg-indigo-600 rounded">Return</router-link>
+    <h1 class="mb-4 text-3xl font-bold text-blue-600 underline">
+      Interaction screen
+    </h1>
+    <InteractionCanvas />
+    <router-link
+      to="/"
+      class="px-6 py-2 text-sm text-green-100 bg-indigo-600 rounded"
+      >Return</router-link
+    >
   </div>
 </template>
+<script setup>
+import InteractionCanvas from "../components/InteractionCanvas.vue";
+</script>

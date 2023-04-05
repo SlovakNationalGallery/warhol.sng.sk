280a9eddb156a29a948743f860ca7be5dd318d0b
Igor Rjabinin <igor.rjabinin@gmail.com>
2023-04-05 18:46:02 +0200

    homescreen with cans + CTA

--- src/views/HomeScreen.vue
+++ src/views/HomeScreen.vue
@@ -1,8 +1,19 @@
 <template>
-  <div class="grid grid-rows-5 grid-cols-2 lg:grid-cols-5 lg:grid-rows-2 gap-4 mx-auto my-8 max-h-screen">
-    <div class="bg-blue-200 rounded-sm flex" v-for="index in 10" :key="index">
-      <img v-if="index < 10" src="https://www.webumenia.sk/dielo/nahlad/SVK:SNG.G_13273/800" :alt="'Campbell\'s Soup ' + index" class="h-full w-auto" width="522" height="800">
-      <router-link v-else to="/create-soup" class="px-6 py-4 text-green-100 bg-indigo-600 rounded-lg text-2xl m-auto  d-block text-center">Create your own <br> can of soup</router-link>
+  <div class="w-full h-full bg-wall">
+    <div class="grid grid-rows-5 grid-cols-2 lg:grid-cols-5 lg:grid-rows-2 !gap-[120px] w-full p-28">
+      <div class="bg-white p-2.5 border-wall drop-shadow-3xl border-[14px] rounded-sm flex" v-for="index in 10" :key="index">
+        <img src="../assets/can.png" :alt="'Campbell\'s Soup ' + index" class="h-full w-auto mx-auto" width="218" height="330" />
+      </div>
     </div>
+    <router-link
+      to="/create-soup"
+      class="px-4 py-4 xl:py-8 text-white bg-black text-2xl font-medium block text-center w-100 absolute inset-x-0 bottom-0"
+    >
+      Create your own can of soup
+      <svg width="63" height="52" viewBox="0 0 63 52" class="w-8 h-auto fill-none stroke-white inline-block ml-2">
+        <path d="M0 26H59" stroke-width="6" />
+        <path d="M35 49L58 26L35 3" stroke-width="6" />
+      </svg>
+    </router-link>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ["./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        wall: {
+          DEFAULT: "#F2F0EA",
+        },
+      },
+      dropShadow: {
+        "3xl": "-4px 18px 32px rgba(87, 81, 64, 0.14)",
+      },
+    },
   },
   plugins: [],
 };

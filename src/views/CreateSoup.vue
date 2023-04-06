a0b2dc035fed8dbd1f443e8cf064965ffe9b86e6
Igor Rjabinin <igor.rjabinin@gmail.com>
2023-04-06 02:03:06 +0200

    add storyline

--- src/views/CreateSoup.vue
+++ src/views/CreateSoup.vue
@@ -1,20 +1,43 @@
 <template>
-  <div>
-    <h1 class="mb-4 text-3xl font-bold text-blue-600 underline">
-      Interaction screen
-    </h1>
-    <PaintCanvas />
-    <StencilCanvas />
-    <LabelCanvas />
-    <router-link
-      to="/"
-      class="px-6 py-2 text-sm text-green-100 bg-indigo-600 rounded"
-      >Return</router-link
-    >
+  <div class="w-full h-full bg-wall xl:flex">
+    <div class="xl:h-full bg-white xl:w-3/5">
+      <StencilCanvas class="mx-auto" />
+    </div>
+    <div class="p-16 xl:w-2/5 min-h-screen relative">
+      <div class="mb-5">krok <b>1 / 5</b></div>
+      <svg class="w-100 h-auto fill-none stroke-black stroke-[4px] mb-16" viewBox="0 0 660 24">
+        <circle cx="12" cy="12" r="10" class="fill-black" />
+        <path d="M24 12H159" />
+        <circle cx="171" cy="12" r="10" />
+        <path d="M183 12H318" />
+        <circle cx="330" cy="12" r="10" />
+        <path d="M342 12H477" />
+        <circle cx="489" cy="12" r="10" />
+        <path d="M501 12H636" />
+        <circle cx="648" cy="12" r="10" />
+      </svg>
+      <h1 class="mb-8 text-2xl xl:text-5xl font-bold font-sng">Prilož a zarovnaj šablonu</h1>
+      <div class="">
+        <p class="mb-4">
+          Text o tomto kroku. Keď spomínam Andyho Warhola isto sa vám vybavia plagáty s paradajkovou polievkou, či známe farebné portréty
+          krásnej pani Marilyn. Mnoho Andyho diel bolo vytvorených práve technikou sieťotlače. Ako však sieťotlačovať Keď spomínam Andyho
+          Warhola isto sa vám vybavia plagáty s paradajkovou polievkou, či známe farebné portréty krásnej pani Marilyn. Mnoho Andyho diel
+          bolo vytvorených práve technikou sieťotlače. Ako však sieťotlačovať
+        </p>
+        <p class="mb-4">
+          Text o tomto kroku. Keď spomínam Andyho Warhola isto sa vám vybavia plagáty s paradajkovou polievkou, či známe farebné portréty
+          krásnej pani Marilyn. Mnoho Andyho diel bolo vytvorených práve technikou sieťotlače. Ako však sieťotlačovať Keď spomínam Andyho
+          Warhola isto sa vám vybavia plagáty s paradajkovou polievkou, či známe farebné portréty krásnej pani Marilyn. Mnoho Andyho diel
+          bolo vytvorených práve technikou sieťotlače. Ako však sieťotlačovať
+        </p>
+      </div>
+      <div class="bottom-0 absolute inset-x-0 flex justify-between p-16">
+          <router-link to="/" class="block uppercase font-sng font-medium text-white bg-black rounded-full text-2xl py-4 px-10">Späť</router-link>
+          <router-link to="/" class="block uppercase font-sng font-medium text-white bg-black rounded-full text-2xl py-4 px-10">Ďalej</router-link>
+      </div>
+    </div>
   </div>
 </template>
 <script setup>
-import StencilCanvas from "../components/StencilCanvas.vue";
-import PaintCanvas from "../components/PaintCanvas.vue";
-import LabelCanvas from "../components/LabelCanvas.vue";
+import StencilCanvas from "../components/StencilCanvas.vue"
 </script>
--- src/views/HomeScreen.vue
+++ src/views/HomeScreen.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="w-full h-full bg-wall">
-    <div class="grid grid-rows-5 grid-cols-2 lg:grid-cols-5 lg:grid-rows-2 gap-4 md:gap-14 2xl:!gap-[120px] w-full py-8 px-16 2xl:p-28">
+  <div class="w-full h-full bg-wall flex flex-col">
+    <div class="grow grid grid-rows-5 grid-cols-2 lg:grid-cols-5 lg:grid-rows-2 gap-4 md:gap-x-14 md:gap-y-10 2xl:!gap-x-[120px] 2xl:!gap-y-[80px] w-full px-16 py-8 2xl:px-28 2xl:py-20">
       <div class="bg-white p-2.5 border-wall drop-shadow-3xl border-[14px] rounded-sm flex" v-for="index in 10" :key="index">
         <img src="../assets/can.png" :alt="'Campbell\'s Soup ' + index" class="h-full w-auto mx-auto" width="218" height="330" />
       </div>
     </div>
     <router-link
       to="/create-soup"
-      class="font-sng px-4 py-4 xl:py-8 hover:text-red group text-white bg-black text-3xl 2xl:text-6xl font-medium block text-center w-100 absolute inset-x-0 bottom-0"
+      class="font-sng px-4 py-4 xl:py-8 hover:text-red group text-white bg-black text-3xl 2xl:text-6xl font-medium block text-center w-100"
     >
       Vytvor si vlastnú polievku
       <svg width="63" height="52" viewBox="0 0 63 52" class="w-8 xl:w-14 h-auto fill-none stroke-white group-hover:stroke-red inline-block ml-2">
